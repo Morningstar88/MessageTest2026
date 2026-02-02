f4ccf4298508a54d375c722d50d8e0e9978388de
https://webchat.freenode.net/#Ubud <jake_snow@tutanota.com>
2026-02-02 17:27:07 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: "2rem" }}>
+      <h1>Hello World from Vercel!</h1>
+      <p>This is the front page, served at /</p>
+    </div>
+  );
+}
